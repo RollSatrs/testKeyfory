2bd95a3c63cad6203e3f2e313ae485d80c004d07
rollsars <rollanoffical05@gmail.com>
2025-08-07 14:25:55 +0500

    error 2.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    react()
+    react(),
+    tailwindcss()
   ],
   server: {
+    host: true,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
